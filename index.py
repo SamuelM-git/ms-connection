44861e54cf15462b4d535aa0b9a0a8001e261313
SamuelM-git <samuel.mathi3@gmail.com>
2025-07-25 00:11:13 +0200

    finalisation de la page de connection et mis en ligne

--- index.py
+++ index.py
@@ -3,6 +3,8 @@
 # 2. Importez le module et créez une instance d'authentification
 import streamlit as st
 from streamlit_authenticator import Authenticate
+from streamlit_option_menu import option_menu
+
 # import streamlit_authenticator as stauth
 
 # Nos données utilisateurs doivent respecter ce format
@@ -13,7 +15,7 @@ lesDonneesDesComptes = {
             'password': 'utilisateurMDP',
             'email': 'utilisateur@gmail.com',
             'failed_login_attemps': 0,  # Sera géré automatiquement
-            'logged_in': False,          # Sera géré automatiquement
+            'logged_in': False,         # Sera géré automatiquement
             'role': 'utilisateur'
         },
         'root': {
@@ -21,7 +23,7 @@ lesDonneesDesComptes = {
             'password': 'rootMDP',
             'email': 'admin@gmail.com',
             'failed_login_attemps': 0,  # Sera géré automatiquement
-            'logged_in': False,          # Sera géré automatiquement
+            'logged_in': False,         # Sera géré automatiquement
             'role': 'administrateur'
         }
     }
@@ -40,19 +42,67 @@ authenticator = Authenticate(
 authenticator.login()
 
 
-# 4. Gérer l'accès en fonction des informations renseignées
+# page d'acceuil apres connection
 def accueil():
-    st.title("Bienvenu sur le contenu réservé aux utilisateurs connectés")
+    # On affiche un menu déroulant (selectbox) DANS la barre latérale (sidebar)
+    st.title("Bienvenu sur ma page")
+    st.image("https://gifdb.com/images/high/standing-ovation-crowd-applause"
+             "photo -oscar-awards-ai72icmh1ac7apdz.gif")
 
 
+# Page des photos
+def chat():
+    st.title("Bienvenu dans l'album des animaux  😻")
+
+    # Création du menu qui va afficher les choix qui se trouvent dans la
+    # variable options
+    # Création de 3 colonnes
+    col1, col2, col3 = st.columns(3)
+
+    # Contenu de la première colonne :
+    with col1:
+        st.image("https://static.streamlit.io/examples/cat.jpg")
+
+    # Contenu de la deuxième colonne :
+    with col2:
+        st.image("https://www.melty.fr/wp-content/uploads/meltyfr/2023/07/1-6-"
+                 "729x410.jpg")
+
+    # Contenu de la troisième colonne :
+    with col3:
+        st.header("Un hiboux")
+        st.image("https://static.streamlit.io/examples/owl.jpg")
+
+
+# Bloc
 if st.session_state["authentication_status"]:
-    accueil()
-# Le bouton de déconnexion
-    authenticator.logout("Déconnexion")
+    # notre bloc qui est à gauche
+    with st.sidebar:
+        # Le bouton de déconnexion
+        authenticator.logout("Déconnexion")
+
+        st.write(f"Bienvenue {st.session_state['name']}")
+
+        # Création du menu qui va afficher les choix qui se trouvent dans la
+        #  variable options
+        selection = option_menu(
+            menu_title=None,
+            options=["🤩 Accueil", "😻 Photos"]
+        )
+
+    # On indique au programme quoi faire en fonction du choix
+    if selection == "🤩 Accueil":
+        accueil()
+    elif selection == "😻 Photos":
+        chat()
+    # ... et ainsi de suite pour les autres pages
 elif st.session_state["authentication_status"] is False:
-
     st.error("L'username ou le password est/sont incorrect")
-
 elif st.session_state["authentication_status"] is None:
-
     st.warning('Les champs username et mot de passe doivent être remplie')
+
+# Bas de page
+st.write('___')
+st.write("<h7 style='text-align: center; color: green;'>© 2025 Samuel M. All"
+         "rights reserved.🦁</h7>", unsafe_allow_html=True)
+st.link_button("LinkedIn", "https://www.linkedin.com/in/samuel-m-co")
