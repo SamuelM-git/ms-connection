c16e5c7e5d4e5b561602c4d946576dfcd843e2d9
SamuelM-git <samuel.mathi3@gmail.com>
2025-07-24 16:06:33 +0200

    ajout code connection

--- index.py
+++ index.py
@@ -1,8 +1,9 @@
 # 1. Installez le module Streamlit Authenticator avec pip
 
 # 2. Importez le module et créez une instance d'authentification
-# import streamlit as st
+import streamlit as st
 from streamlit_authenticator import Authenticate
+# import streamlit_authenticator as stauth
 
 # Nos données utilisateurs doivent respecter ce format
 lesDonneesDesComptes = {
@@ -26,9 +27,32 @@ lesDonneesDesComptes = {
     }
 }
 
+
 authenticator = Authenticate(
     lesDonneesDesComptes,  # Les données des comptes
     "cookie name",         # Le nom du cookie, un str quelconque
     "cookie key",          # La clé du cookie, un str quelconque
     30,                    # Le nombre de jours avant que le cookie expire
 )
+
+# 3. Utiliser la méthode login pour afficher le formulaire de connexion et
+# vérifier les informations d'identification de l'utilisateur
+authenticator.login()
+
+
+# 4. Gérer l'accès en fonction des informations renseignées
+def accueil():
+    st.title("Bienvenu sur le contenu réservé aux utilisateurs connectés")
+
+
+if st.session_state["authentication_status"]:
+    accueil()
+# Le bouton de déconnexion
+    authenticator.logout("Déconnexion")
+elif st.session_state["authentication_status"] is False:
+
+    st.error("L'username ou le password est/sont incorrect")
+
+elif st.session_state["authentication_status"] is None:
+
+    st.warning('Les champs username et mot de passe doivent être remplie')
